523fc2b2fe3a7f9b33b226e7f5b0fc354ece1a10
Alessandro Chumpitaz <par_214@hotmail.com>
2023-04-03 13:37:42 -0500

    :bug: Fixed chart offset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "nuxt-lodash"],
+  app: {
+    head: {
+      title: "Traductores e intérpretes de lenguas indígenas",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+    },
+  },
   runtimeConfig: {
     public: {
       apiUrl: process.env.INTERPRETERS_API_URL!,
       env: process.env.NODE_ENV!,
     },
   },
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "nuxt-lodash"],
 });
